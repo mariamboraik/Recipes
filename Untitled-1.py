053bdf22b6b4c8ca53b52b2e6b4172502838cc85
mariamboraik <mariamhamdy7070@gmail.com>
2024-11-13 15:54:36 +0200

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,9 @@
+print("Welcome to the Simple Calculator (First Release)!")
+print("This calculator currently only supports addition.")
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+sum_result = num1 + num2
+
+print(f"{num1} + {num2} = {sum_result}") 
